d8902c640aae2f1c5ec1fd23fa12878a7e80f054
Callum Flack <callumflack@gmail.com>
2016-04-21 13:28:13 +1000

    begin styling the survey

--- app/components/ui-Survey/Question.js
+++ app/components/ui-Survey/Question.js
@@ -2,26 +2,27 @@ var React = require('react');
 var Answer = require('./Answer.js');
 
 var Question = React.createClass({
-    getInitialState: function() {
-        return {answered : false};
-    },
-    handleChildChange: function(child) {
-        console.log("A child has changed!");
-    },
-    render: function() {
-        var questionNumber = this.props.questionData.number;
-        return (
-            <div className={"Survey-question " + this.props.answered? "-is-answered" : ""}>
-                <span>Q{this.props.questionData.number}</span>
-                <span>{this.props.questionData.text}</span>
-                <div className="Survey-questionAnswers">
-                    {this.props.questionData.answers.map(function(answer, i) {
-                        return <Answer key={i} value={answer.value} label={answer.label} questionNumber={questionNumber} question={this}/>
-                    })}
-                </div>
-            </div>
-        )
-    }
+	getInitialState: function() {
+		return {answered : false};
+	},
+	handleChildChange: function(child) {
+		console.log("A child has changed!");
+	},
+	render: function() {
+		var questionNumber = this.props.questionData.number;
+		return (
+			<div className="c-question">
+				<div className={"Survey-question " + this.props.answered? "-is-answered" : ""}></div>
+				<span>Q{this.props.questionData.number}</span>
+				<span>{this.props.questionData.text}</span>
+				<div className="Survey-questionAnswers">
+					{this.props.questionData.answers.map(function(answer, i) {
+						return <Answer key={i} value={answer.value} label={answer.label} questionNumber={questionNumber} question={this}/>
+					})}
+				</div>
+			</div>
+		)
+	}
 })
 
 module.exports = Question;
--- app/components/ui-Survey/Survey.js
+++ app/components/ui-Survey/Survey.js
@@ -1,18 +1,21 @@
+var React = require("react");
+var Nav = require("../ui-Nav/Nav.js");
 var questionData = require('./data/questions.js');
-var React = require('react');
 var Question = require('./Question');
 
+require('./../../stylesheets/app.scss');
+
 var Survey = React.createClass({
-    render: function() {
-        return (
-            <form method="post" action="" className="Survey">
-                {this.props.questionData.map(function(questionData, i) {
-                  return <Question questionData={questionData} key={i}/>  
-                })}
-                <input type="submit" value="Submit survey"/>
-            </form>
-        )
-    }
+	render: function() {
+		return (
+			<form method="post" action="" className="Survey u-marginT5">
+				{this.props.questionData.map(function(questionData, i) {
+				  return <Question questionData={questionData} key={i}/>
+				})}
+				<input type="submit" value="Submit survey"/>
+			</form>
+		)
+	}
 });
 
 
