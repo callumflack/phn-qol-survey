8c71015e598e6390f3974685ac0083338a6ab735
Callum Flack <callumflack@gmail.com>
2016-04-22 13:52:49 +1000

    first attempts at classname manipulations via state/props

--- app/components/ui-Survey/Answer.js
+++ app/components/ui-Survey/Answer.js
@@ -2,15 +2,39 @@ var React = require('react');
 var IconChoice = require('../ui-Icons/Icons.js').IcChoice;
 
 var Answer = React.createClass({
+	getInitialState: function() {
+		return {
+			chosen : false
+		};
+	},
+
+	handleClick: function(event) {
+		this.setState({
+			chosen : !this.state.chosen
+		});
+	},
+
 	updateQuestion: function(event) {
 	},
-	
+
 	render: function() {
+		var choiceClassName = "c-question-choices--option";
+		var iconActive = " ";
+
+		if (this.state.chosen) {
+			choiceClassName += " is-active";
+			iconActive += "is-active";
+			this.props.value = "checked";
+		}
+
 		return (
-			<div className="c-question-choices--option " onClick={this.updateQuestion}>
+			<div className={choiceClassName} onClick={this.handleClick}>
 				<input name={"q_" + this.props.questionNumber} value={this.props.value} type="radio"/>
-				<label className="">{this.props.label}</label>
-				<IconChoice />
+				<label className="">
+					<span className="c-radioInputNumber">1</span>
+					{this.props.label}
+				</label>
+				<IconChoice active={iconActive} />
 			</div>
 		)
 	}
