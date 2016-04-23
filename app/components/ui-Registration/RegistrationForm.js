ca94cf9d85bb748a2f3614cace89bd95dd921b85
Callum Flack <callumflack@gmail.com>
2016-04-23 12:42:26 +1000

    remove value prop so that react doesn't master it (and you can thus type into the field in the DOM)

--- app/components/ui-Registration/RegistrationForm.js
+++ app/components/ui-Registration/RegistrationForm.js
@@ -15,7 +15,7 @@ var RegistrationForm = React.createClass({
 		<form className="u-marginT3 u-flex" id="" action="" method="post" name="registration-form">
 			<div className="Form-group u-flexGrow1 u-paddingRD1">
 				<label for=""></label>
-				<input className="Form-control required" type="text" value="" name="" placeholder="Add your service provider code" required></input>
+				<input className="Form-control required" type="text" name="" placeholder="Add your service provider code" required></input>
 			</div>
 			<div className="Form-group u-flexExpandLeft">
 				<input className="Button t-button t-button--md" type="submit" value="Register" name="" />
