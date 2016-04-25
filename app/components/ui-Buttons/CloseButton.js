6bbd86db010a31e416c6ae187378b6bc55f0c419
Callum Flack <callumflack@gmail.com>
2016-04-25 15:26:20 +1000

    move CloseButton component into Buttons dir

--- app/components/ui-Buttons/CloseButton.js
+++ app/components/ui-Buttons/CloseButton.js
@@ -2,17 +2,20 @@ var React = require("react");
 var IcClose = require('../ui-Icons/Icons.js').IcClose;
 var classNames = require('classnames');
 
+// Close btn in same file to study flow of state/props!
 var CloseButton = React.createClass({
 	getInitialState: function() {
-		return {liked: false};
+		return { open: false };
 	},
 
 	handleClick: function(event) {
-		this.setState({liked: !this.state.liked});
+		/* set state to 'not open' */
+		this.setState({ open: !this.state.open });
 	},
 
 	render: function () {
 		return (
+			/* bind this state to onClick w/ something like {this.click.bind(this)} ?? */
 			<button className="t-buttonClose" onClick="" aria-label="close">
 				<IcClose size="super" />
 			</button>
