77d4c1cfb8ccc314fd9eaf76bd0a2df44eadf000
Callum Flack <callumflack@gmail.com>
2016-04-26 17:43:19 +1000

    incomplete attempt at form floating label pattern

--- app/components/ui-Registration/RegistrationForm.js
+++ app/components/ui-Registration/RegistrationForm.js
@@ -6,15 +6,40 @@ var RegistrationForm = React.createClass({
 		console.log("Supressing form submit.!");
 		submitEvent.preventDefault();
 		submitEvent.stopPropagation();
-		
+
 		return false;
 	},
+
+	getInitialState: function() {
+		return {
+			activateForm: false
+		};
+	},
+
+	handleClick: function(event) {
+		this.setState({
+			activateForm: !this.state.activateForm
+		}, this.focusAfterClick);
+	},
+
+	focusAfterClick: function() {
+		if (this.state.activateForm)
+			this.refs.textField.getDOMNode().focus();
+	},
+
 	render: function () {
+		var formGroupClasses = classNames({
+			'Form-group': true,
+			'u-flexGrow1': true,
+			'u-paddingRD1': true,
+			'is-active': this.state.activateForm
+		});
+
 		return (
 			<form className="u-marginT3 u-flex" method="post" name="registration-form" onSubmit={this.registerDevice}>
-				<div className="Form-group u-flexGrow1 u-paddingRD1">
-					<label for="code">Your service provider code</label>
-					<input className="Form-control required" id="code" type="text" name="providerCode" placeholder="" required></input>
+				<div className={formGroupClasses} onClick={this.handleClick}>
+					<label for="providerCode">Your service provider code</label>
+					<input className="Form-control" refs="textField" type="text" name="providerCode" required></input>
 				</div>
 				<div className="Form-group u-flexExpandLeft">
 					<input className="Button t-button t-button--md" type="submit" value="Register" name="submit" />
