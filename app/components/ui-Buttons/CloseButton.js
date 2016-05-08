cd15381577d3b600383bee14efe1521648cdf8ee
Callum Flack <callumflack@gmail.com>
2016-05-08 16:45:04 +1000

    cleanup unreq'd comments & var

--- app/components/ui-Buttons/CloseButton.js
+++ app/components/ui-Buttons/CloseButton.js
@@ -1,8 +1,6 @@
 var React = require("react");
 var IcClose = require('../ui-Icons/Icons.js').IcClose;
-var classNames = require('classnames');
 
-// Close btn in same file to study flow of state/props!
 var CloseButton = React.createClass({
 	render: function () {
 		return (
