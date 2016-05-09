2c59139528d4a131f205bad35728be53e8b51822
Callum Flack <callumflack@gmail.com>
2016-05-09 17:33:07 +1000

    move nav logo into ui-Elements for reuse

---
+++ app/components/ui-Elements/NavLogo.js
@@ -0,0 +1,14 @@
+var React = require("react");
+
+var NavLogo = React.createClass({
+	render: function () {
+
+		return (
+			<span className="c-nav-home">
+				<img src="images/NQPHN.png" alt="Primary Health Network Northern Queensland" />
+			</span>
+		);
+	}
+});
+
+module.exports = NavLogo;
---
+++ app/components/ui-Elements/NavLogo.test.js
@@ -0,0 +1,6 @@
+var React = require('react');
+
+const NavLogo = () =>
+	<span className="c-nav-home">
+		<img src="images/NQPHN.png" alt="Primary Health Network Northern Queensland" />
+	</span>
