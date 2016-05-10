2487748c4d8d9f05578b5665004e3c6a2e4673ca
Callum Flack <callumflack@gmail.com>
2016-05-11 00:31:15 +1000

    add download btn component for admin page

---
+++ app/components/ui-Admin/DownloadButtons.js
@@ -0,0 +1,18 @@
+var React = require("react");
+var IcAvatar = require('../ui-Elements/Icons.js').IcAvatar;
+var IcDownload = require('../ui-Elements/Icons.js').IcDownload;
+
+var DownloadButtons = React.createClass({
+	render: function () {
+		return (
+			<div className="u-textCenter u-marginT15">
+				<button className="Button t-button t-button--full" type="submit" value="" name="">
+					<IcDownload align="left" />Download
+				</button>
+			</div>
+
+		);
+	}
+});
+
+module.exports = DownloadButtons;
